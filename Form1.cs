97603b8d756c2a7c1e62e592176b2e4ba5b76348
diego7016 <diego.areiza114@pascualbravo.edu.co>
2025-09-24 19:40:52 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Gestion_de_informacion_de_una_clinica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
